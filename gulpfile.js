cc42f5d33726081e4e337442ae47d3c357001128
Fajkowsky <dawidfajkowski@gmail.com>
2016-12-12 21:26:20 +0100

    Strings must use doublequote

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
-var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    htmlmin = require('gulp-htmlmin'),
-    server = require('gulp-server-livereload'),
-    debug = require('gulp-debug'),
-    cleanCSS = require('gulp-clean-css'),
-    pump = require('pump');
+var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    htmlmin = require("gulp-htmlmin"),
+    server = require("gulp-server-livereload"),
+    debug = require("gulp-debug"),
+    cleanCSS = require("gulp-clean-css"),
+    pump = require("pump");
 
 var config = {
-    dist: './dist/',
+    dist: "./dist/",
     libs: {
         js: {
             dirs: [
-                'bower_components/angular/angular.js',
-                'bower_components/angular-animate/angular-animate.js',
-                'bower_components/angular-aria/angular-aria.js',
-                'bower_components/angular-material/angular-material.js'
+                "bower_components/angular/angular.js",
+                "bower_components/angular-animate/angular-animate.js",
+                "bower_components/angular-aria/angular-aria.js",
+                "bower_components/angular-material/angular-material.js"
             ],
-            name: 'libs.min.js'
+            name: "libs.min.js"
         },
         css: {
             dirs: [
-                'bower_components/angular-material/angular-material.css'
+                "bower_components/angular-material/angular-material.css"
             ],
-            name: 'libs.min.css'
+            name: "libs.min.css"
         }
     },
     app: {
         js: {
-            dirs: 'app/**/*.js',
-            name: 'app.min.js'
+            dirs: "app/**/*.js",
+            name: "app.min.js"
         }
     }
 };
@@ -44,7 +44,7 @@ var css = [
 ];
 
 
-gulp.task('js', function () {
+gulp.task("js", function () {
     js.forEach(function (item) {
         pump([
             gulp.src(item.src),
@@ -55,26 +55,26 @@ gulp.task('js', function () {
     });
 });
 
-gulp.task('css', function () {
+gulp.task("css", function () {
     css.forEach(function (item) {
         pump([
             gulp.src(item.src),
-            cleanCSS({compatibility: 'ie8'}),
+            cleanCSS({compatibility: "ie8"}),
             concat(item.name),
             gulp.dest(item.dest)
         ]);
     });
 });
 
-gulp.task('html', function () {
+gulp.task("html", function () {
     pump([
-        gulp.src('app/index.html'),
+        gulp.src("app/index.html"),
         htmlmin({collapseWhitespace: true}),
         gulp.dest(config.dist)
     ]);
 });
 
-gulp.task('webserver', function () {
+gulp.task("webserver", function () {
     pump([
         gulp.src(config.dist),
         server({
@@ -83,12 +83,12 @@ gulp.task('webserver', function () {
     ]);
 });
 
-gulp.task('watch', function () {
-    gulp.watch('app/**/*.html', ['html']);
-    gulp.watch('app/**/*.js', ['js']);
-    gulp.watch('app/**/*.css', ['css']);
+gulp.task("watch", function () {
+    gulp.watch("app/**/*.html", ["html"]);
+    gulp.watch("app/**/*.js", ["js"]);
+    gulp.watch("app/**/*.css", ["css"]);
 });
 
-gulp.task('default', ['prod']);
-gulp.task('prod', ['js', 'css', 'html']);
-gulp.task('dev', ['prod', 'webserver', 'watch']);
+gulp.task("default", ["prod"]);
+gulp.task("prod", ["js", "css", "html"]);
+gulp.task("dev", ["prod", "webserver", "watch"]);
